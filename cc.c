26a0fdada9ca57407e4eb9379e3eb96010ce48ca
Fedjmike <sam.nipps@gmail.com>
2015-05-29 20:44:25 +0100

    Use <> for system headers

--- cc.c
+++ cc.c
@@ -1,7 +1,7 @@
-#include "stdlib.h"
-#include "string.h"
-#include "ctype.h"
-#include "stdio.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
 
 char* strdup (char* str);
 
