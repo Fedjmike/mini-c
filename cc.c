5a86b0fdc28b1943680efc27d57fa18203eddb02
Fedjmike <fedjmike@gmail.com>
2017-04-13 21:02:16 +0200

    Local variable space allocation must occur in a code path that is always taken

--- cc.c
+++ cc.c
@@ -556,18 +556,11 @@ void line () {
 }
 
 void function (char* ident) {
-    //Prologue
-
-    fprintf(output, ".globl %s\n", ident);
-    fprintf(output, "%s:\n", ident);
-
-    fputs("push ebp\n"
-          "mov ebp, esp\n", output);
-
     //Body
-
+    
+    int body = new_label();
+    fprintf(output, "_%08d:\n", body);
     return_to = new_label();
-
     line();
 
     //Epilogue
@@ -576,6 +569,16 @@ void function (char* ident) {
     fputs("mov esp, ebp\n"
           "pop ebp\n"
           "ret\n", output);
+    
+    //Prologue
+    //Only after passing the body do we know how much space to allocate for the
+    //local variables, so we write the prologue here at the end.
+    fprintf(output, ".globl %s\n"
+                    "%s:\n", ident, ident);
+    fprintf(output, "push ebp\n"
+                    "mov ebp, esp\n"
+                    "sub esp, %d\n"
+                    "jmp _%08d\n", local_no*word_size, body);
 }
 
 void decl (int kind) {
@@ -624,7 +627,6 @@ void decl (int kind) {
     } else {
         if (kind == decl_local) {
             local = new_local(ident);
-            fprintf(output, "sub esp, %d\n", word_size);
 
         } else
             (kind == decl_module ? new_global : new_param)(ident);
